74f50c484e4cb2c844b565e4c9c5906cf5f4d28c
Klayton <kliekkio@gmail.com>
2017-08-21 14:54:20 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,48 @@
 // create variables for 
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
+
 
 // generate and display a random number between 19-120 in the "scorebox"
-function updateDom() {
-    document.querySelector("#Wins").innerHTML = wins;
-    document.querySelector("#Losses").innerHTML = losses
-}
+
+// function randomNumber(min, max) {
+// 	return Math.floor((Math.random() * max) + min);
+// }
+// var crystals = [];
+
+// function crystalArray() {
+// 	crystals = [];	
+// 	for (var i = 0; i < 4; i++) {
+// 		crystals.push();
+// 		}
+// 	}		
 
 
-function reset () {
-	numDisplayC1 = 0;
-	numDisplayC2 = 0;
-	numDisplayC3 = 0;
-	numDisplayC4 = 0;
 
-}
 
 $(document).ready(function() {
+	var wins = 0;
+	var losses = 0;
+	var totalScore = 0;
+	var targetScore = Math.floor(Math.random() * 120) +19; 
+	var numDisplayC1 = Math.floor(Math.random() * 12) +1;
+	var numDisplayC2 = Math.floor(Math.random() * 12) +1;
+	var numDisplayC3 = Math.floor(Math.random() * 12) +1;
+	var numDisplayC4 = Math.floor(Math.random() * 12) +1;
+
+	function resetHtml() {
+
+	$("#targetScore").text(targetScore);
+	$("#wins").text("Wins " + wins);
+	$("#losses").text("Losses " + losses);
+	$("#total-score").text(totalScore);
+	}
+
+
 
       $("#random-button").on("click", function() {
-          var targetScore = Math.floor(Math.random() * 120) +19;   
+          targetScore = Math.floor(Math.random() * (120-19)) +19;   
+          totalScore = 0;
           $("#score-box").html(targetScore);
+          resetHtml();
       });
 
 
@@ -30,45 +50,56 @@ $(document).ready(function() {
 // total score box
 // give each crystal image a random number between 1-12 and make it hidden
         $("#crystal1").on("click", function() {
-      		var numDisplayC1 = Math.floor(Math.random() * 12) +1;
-      		$("#total-score").html(numDisplayC1);
-  			console.log(numDisplayC1);
- 			reset(); 
+        	totalScore = numDisplayC1 + totalScore;
+      		$("#total-score").html(totalScore);
+  			
+ 			check();
 		});			
   			    		
 		$("#crystal2").on("click", function() {
-      		var numDisplayC2 = Math.floor(Math.random() * 12) +1;
-      		$("#total-score").html(numDisplayC2);
-      		console.log(numDisplayC2);
-      		reset();
+			totalScore = numDisplayC2 + totalScore;
+      		$("#total-score").html(totalScore);
+      		
+      		check();
       	});
 
       	$("#crystal3").on("click", function() {
-      		var numDisplayC3 = Math.floor(Math.random() * 12) +1;
-      		$("#total-score").html(numDisplayC3);
-      		console.log(numDisplayC3);
-      		reset();
+      		totalScore = numDisplayC3 + totalScore;
+      		$("#total-score").html(totalScore);
+      		
+      		check();
   		});
 
       	$("#crystal4").on("click", function() {
-      		var numDisplayC4 = Math.floor(Math.random() * 12) +1;
-      		$("#total-score").html(numDisplayC4);
-      		console.log(numDisplayC4);
-      		reset();
+      		totalScore = numDisplayC4 + totalScore;
+      		$("#total-score").html(totalScore);
+      		
+      		check();
   		});
       		
       	
 
 // if the player matches the displayed random number than increase wins
 // by 1
+	var check = function() {
+		console.log(totalScore);
+		console.log(targetScore);
 		if (totalScore === targetScore) {
 			wins++;
+			$("#wins").html("Wins " + wins);
+			alert("You win!");
+
+
+
 		} else if (totalScore > targetScore) {
 			losses++;
+			$("#losses").html("Losses " + losses);
+			alert("You lose!");
 		}
 
-	updateDom();
 
+}
+	
 // if the player goes over the random number displayed, increase losses by 1
 
 // when either a win or a loss is recorded, reset the randomly displayed
